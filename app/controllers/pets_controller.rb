10e21171f311682f4923d9030d005936f430451c
Jeshua Hicks <jhicks7884@outlook.com>
2020-01-15 13:36:26 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -4,12 +4,12 @@ class PetsController < ApplicationController
       @pets = Pet.all
       erb :'/pets/index'
     end
-  
+
     get '/pets/new' do
       @owners = Owner.all
       erb :'/pets/new'
     end
-  
+
     post '/pets' do
       @pet = Pet.create(params[:pet])
       if !params[:owner][:name].empty?
@@ -20,20 +20,20 @@ class PetsController < ApplicationController
       @pet.save
       redirect to "pets/#{@pet.id}"
     end
-  
+
     get '/pets/:id/edit' do
       @pet = Pet.find_by_id(params[:id])
       @owners = Owner.all
       erb :'/pets/edit'
     end
-  
+
     get '/pets/:id' do
       @pet = Pet.find(params[:id])
       erb :'/pets/show'
     end
-  
+
     post '/pets/:id' do
-  
+
       redirect to "pets/#{@pet.id}"
     end
   end
